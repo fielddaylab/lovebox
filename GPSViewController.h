fc4163affdfed6638b178f9da020c1212065082e
mrdavidgagnon <mrdavidgagnon@gmail.com>
2010-07-01 04:38:14 +0000

    Round One of Bike Box customizations.

---
+++ GPSViewController.h
@@ -0,0 +1,37 @@
+//
+//  GPSViewController.h
+//  ARIS
+//
+//  Created by Ben Longoria on 2/11/09.
+//  Copyright 2009 University of Wisconsin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppModel.h"
+#import "Location.h"
+#import <MapKit/MapKit.h>
+#import "Annotation.h"
+
+
+
+@interface GPSViewController : UIViewController <MKMapViewDelegate> {
+	AppModel *appModel;
+	NSArray *locations;
+	BOOL autoCenter;
+	IBOutlet MKMapView *mapView;
+	IBOutlet UIBarButtonItem *mainButton;
+	BOOL silenceNextServerUpdate;
+
+}
+
+-(void) refresh;
+-(void) zoomAndCenterMap;
+- (IBAction)mainButtonTouchAction;
+
+
+@property BOOL autoCenter;
+@property (nonatomic, retain) NSArray *locations;
+@property (nonatomic, retain) IBOutlet MKMapView *mapView;
+@property (nonatomic, retain) IBOutlet UIBarButtonItem *mainButton;
+
+@end
